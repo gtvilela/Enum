e6a56682a2afd0b2e20398af5c669b440fd06798
Gabriela Vilela <gabrielathalitasv@gmail.com>
2019-04-29 13:58:57 -0300

    Definicao Enum

---
+++ Enum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+
+    class Order
+    {
+       public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
